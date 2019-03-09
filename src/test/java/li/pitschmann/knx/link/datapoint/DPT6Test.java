93ecdcc2ffc1653c3e63a9f4c5c54169363f3509
pitschr <github@pitschmann.li>
2019-03-09 19:09:41 +0100

    Separate DPT6 and DPT6.StatusTest

--- src/test/java/li/pitschmann/knx/link/datapoint/DPT6Test.java
+++ src/test/java/li/pitschmann/knx/link/datapoint/DPT6Test.java
@@ -19,10 +19,8 @@
 package li.pitschmann.knx.link.datapoint;
 
 import li.pitschmann.knx.link.datapoint.value.DPT6Value;
-import li.pitschmann.knx.link.datapoint.value.DPT6Value.StatusMode.Mode;
 import li.pitschmann.knx.link.exceptions.DataPointTypeIncompatibleBytesException;
 import li.pitschmann.knx.link.exceptions.DataPointTypeIncompatibleSyntaxException;
-import li.pitschmann.utils.ByteFormatter;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -106,70 +104,4 @@ public class DPT6Test extends AbstractDataPointTypeTest<DPT6, DPT6Value> {
         assertThat(dpt.toByteArray(intValue)).containsExactly(bValue);
     }
 
-    /**
-     * Test Class for {@link DPT6.StatusMode}
-     *
-     * @author PITSCHR
-     */
-    public static class StatusModeTest extends AbstractDataPointTypeTest<DPT6.StatusMode, DPT6Value.StatusMode> {
-        @Override
-        @Test
-        public void testIdAndDescription() {
-            final var dpt = DPT6.STATUS_MODE;
-
-            assertThat(dpt.getId()).isEqualTo("6.020");
-            assertThat(dpt.getDescription()).isEqualTo("Status Mode");
-        }
-
-        @Override
-        @Test
-        public void testCompatibility() {
-            final var dpt = DPT6.STATUS_MODE;
-
-            // failures
-            assertThatThrownBy(() -> dpt.toValue(new byte[2])).isInstanceOf(DataPointTypeIncompatibleBytesException.class);
-            assertThatThrownBy(() -> dpt.toValue(new String[0])).isInstanceOf(DataPointTypeIncompatibleSyntaxException.class);
-            assertThatThrownBy(() -> dpt.toValue(new String[]{"foo"})).isInstanceOf(DataPointTypeIncompatibleSyntaxException.class);
-
-            // OK
-            assertThat(dpt.toValue(new byte[]{0x00})).isInstanceOf(DPT6Value.StatusMode.class);
-            assertThat(dpt.toValue(new byte[]{(byte) 0xFF})).isInstanceOf(DPT6Value.StatusMode.class);
-            assertThat(dpt.toValue(new String[]{"0xFF"})).isInstanceOf(DPT6Value.StatusMode.class);
-        }
-
-        @Override
-        @Test
-        public void testOf() {
-            // mode: 0, no flags set
-            this.assertInternal((byte) 0x01, false, false, false, false, false, DPT6Value.StatusMode.Mode.MODE_0);
-            // mode: 1, no flags set
-            this.assertInternal((byte) 0x02, false, false, false, false, false, DPT6Value.StatusMode.Mode.MODE_1);
-            // mode: 2, no flags set
-            this.assertInternal((byte) 0x04, false, false, false, false, false, DPT6Value.StatusMode.Mode.MODE_2);
-        }
-
-        /**
-         * Asserts the {@link DPT6Value} for given arguments {@link DPT6.StatusMode}, {@code byteValue} and status mode
-         * value relevant parameters
-         *
-         * @param byteValue
-         * @param bool1
-         * @param bool2
-         * @param bool3
-         * @param bool4
-         * @param bool5
-         * @param mode
-         */
-        private void assertInternal(final byte byteValue, final boolean bool1, final boolean bool2, final boolean bool3, final boolean bool4,
-                                    final boolean bool5, final Mode mode) {
-            final var dptStatusMode = DPT6.STATUS_MODE;
-            final var dptStatusModeValue = dptStatusMode.toValue(bool1, bool2, bool3, bool4, bool5, mode);
-
-            // assert base DPT
-            this.assertBaseDPT(dptStatusMode, new byte[]{byteValue}, dptStatusModeValue);
-            // assert specific DPT6
-            assertThat(dptStatusMode.toValue(new String[]{ByteFormatter.formatHex(byteValue)})).isEqualTo(dptStatusModeValue);
-            assertThat(dptStatusMode.toByteArray(bool1, bool2, bool3, bool4, bool5, mode)).containsExactly(byteValue);
-        }
-    }
 }
