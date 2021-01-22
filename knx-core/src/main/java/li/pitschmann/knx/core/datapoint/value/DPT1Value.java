a81a2ca472ea4d307ce0075c53c39aa9d18bfe31
pitschr <github@pitschmann.li>
2021-01-22 23:11:44 +0100

    More test cases for DPT1 and DPT1Value is now more stricter with "true" (e.g. 0000 0011 should still be "false")

--- knx-core/src/main/java/li/pitschmann/knx/core/datapoint/value/DPT1Value.java
+++ knx-core/src/main/java/li/pitschmann/knx/core/datapoint/value/DPT1Value.java
@@ -1,6 +1,6 @@
 /*
  * KNX Link - A library for KNX Net/IP communication
- * Copyright (C) 2019 Pitschmann Christoph
+ * Copyright (C) 2021 Pitschmann Christoph
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -46,7 +46,7 @@ public final class DPT1Value extends AbstractDataPointValue<DPT1> implements Pay
         this(
                 dpt,
                 // boolean
-                (b & 0x01) != 0x00
+                b == 0x01
         );
     }
 
--- knx-core/src/test/java/li/pitschmann/knx/core/datapoint/DPT1Test.java
+++ knx-core/src/test/java/li/pitschmann/knx/core/datapoint/DPT1Test.java
@@ -1,6 +1,6 @@
 /*
  * KNX Link - A library for KNX Net/IP communication
- * Copyright (C) 2019 Pitschmann Christoph
+ * Copyright (C) 2021 Pitschmann Christoph
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -62,18 +62,50 @@ class DPT1Test {
     @DisplayName("Test #parse(byte[])")
     void testByteParse() {
         final var dpt = DPT1.SWITCH;
-        assertThat(dpt.parse(new byte[]{0x00})).isInstanceOf(DPT1Value.class);
-        assertThat(dpt.parse(new byte[]{0x01})).isInstanceOf(DPT1Value.class);
+
+        // 0x01 = 1 => true
+        final var trueByte = dpt.parse(new byte[]{0x01});
+        assertThat(trueByte.getValue()).isTrue();
+
+        // all others are "false" per default
+
+        // 0x00 = 0 => false
+        final var falseByte = dpt.parse(new byte[]{0x00});
+        assertThat(falseByte.getValue()).isFalse();
+        // 0x00 = 0 => false
+        final var falseByte2 = dpt.parse(new byte[]{0x02});
+        assertThat(falseByte2.getValue()).isFalse();
     }
 
     @Test
     @DisplayName("Test #parse(String[])")
     void testStringParse() {
         final var dpt = DPT1.SWITCH;
-        assertThat(dpt.parse(new String[]{"false"})).isInstanceOf(DPT1Value.class);
-        assertThat(dpt.parse(new String[]{"true"})).isInstanceOf(DPT1Value.class);
-        assertThat(dpt.parse(new String[]{"0"})).isInstanceOf(DPT1Value.class);
-        assertThat(dpt.parse(new String[]{"1"})).isInstanceOf(DPT1Value.class);
+
+        // TruE => true
+        final var trueString = dpt.parse(new String[]{"TruE"});
+        assertThat(trueString.getValue()).isTrue();
+        // 1 => true
+        final var trueInt = dpt.parse(new String[]{"1"});
+        assertThat(trueInt.getValue()).isTrue();
+        // On => true
+        final var trueValueText = dpt.parse(new String[]{"ON"});
+        assertThat(trueValueText.getValue()).isTrue();
+
+        // all others are as "false" per default
+
+        // FaLsE => false
+        final var falseString = dpt.parse(new String[]{"FaLsE"});
+        assertThat(falseString.getValue()).isFalse();
+        // 0 => false
+        final var falseInt = dpt.parse(new String[]{"0"});
+        assertThat(falseInt.getValue()).isFalse();
+        // Off => false
+        final var falseValueText = dpt.parse(new String[]{"OFF"});
+        assertThat(falseValueText.getValue()).isFalse();
+        // foobar => false
+        final var falseDefault = dpt.parse(new String[]{"foobar"});
+        assertThat(falseDefault.getValue()).isFalse();
     }
 
     @Test
@@ -82,7 +114,7 @@ class DPT1Test {
         // Switch
         assertThat(DPT1.SWITCH.getTextFor(true)).isEqualTo("On");
         assertThat(DPT1.SWITCH.getTextFor(false)).isEqualTo("Off");
-        // Alaram
+        // Alarm
         assertThat(DPT1.ALARM.getTextFor(true)).isEqualTo("Alarm");
         assertThat(DPT1.ALARM.getTextFor(false)).isEqualTo("No Alarm");
     }
