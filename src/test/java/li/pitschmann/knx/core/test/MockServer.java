6e3b5bc784c5b5d52fd9d3cac8501a13e7ea9cbb
pitschr <github@pitschmann.li>
2021-02-19 01:22:13 +0100

    add println for debugging on CI

--- src/test/java/li/pitschmann/knx/core/test/MockServer.java
+++ src/test/java/li/pitschmann/knx/core/test/MockServer.java
@@ -261,7 +261,6 @@ public final class MockServer implements Runnable, Closeable {
      * @return {@code true} if cancelled/interrupted, otherwise {@code false}
      */
     public boolean isCancelled() {
-        System.out.println("PITSCHR cancel: " + cancel + ", interrupted: " + Thread.currentThread().isInterrupted());
         return cancel || Thread.currentThread().isInterrupted();
     }
 
@@ -304,7 +303,7 @@ public final class MockServer implements Runnable, Closeable {
     public boolean waitForReceivedServiceType(final ServiceType serviceType, final int occurrence) {
         final var notInterrupted = Sleeper.milliseconds(100, () -> !isCancelled() && this.contains(new ArrayList<>(this.receivedBodies), serviceType, occurrence), 30000);
 
-        System.out.println("PITSCHR: notInterrupted: " + notInterrupted);
+        System.out.println("PITSCHR: notInterrupted: " + notInterrupted + ", cancel: " + cancel + ", interrupted: " + Thread.currentThread().isInterrupted());
         if (!notInterrupted) {
             log.error("It took too long for 'waitForReceivedServiceType', here are bodies which were received/sent:\n" +
                     "Received bodies:\n" +
